6db03d5f6220b219ce9ba33161eee9b70d1cad58
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-05 19:23:18 +0600

    fixed issues

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Importing requirements
 const express = require("express");
-const admin = require("firebase-admin");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -11,27 +10,6 @@ const port = process.env.PORT || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json());
-
-// Firebase Admin
-var serviceAccount = require("./nanote-moeen-firebase-adminsdk-nzw53-5acb1929f8.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-// Verifying user
-async function verifyToken(req, res, next) {
-  if (req.headers?.authorization?.startsWith("Bearer ")) {
-    const idToken = req.headers.authorization.split("Bearer ")[1];
-    try {
-      const decodedUser = await admin.auth().verifyIdToken(idToken);
-      req.decodedUserEmail = decodedUser.email;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  next();
-}
 
 //Mongo Client
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nanotecluster.xaaaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -48,16 +26,10 @@ async function run() {
     const favouriteCollection = database.collection("favourites");
 
     //GET API
-    app.get("/notes", verifyToken, async (req, res) => {
-      const email = req.query.email;
-      if (req.decodedUserEmail === email) {
-        const query = { email: email };
-        const cursor = noteCollection.find(query);
-        const notes = await cursor.toArray();
-        res.json(notes);
-      } else {
-        res.status(401).send("User is not authorized");
-      }
+    app.get("/notes", async (req, res) => {
+      const cursor = noteCollection.find({});
+      const notes = await cursor.toArray();
+      res.json(notes);
     });
 
     //POST API
